811e7d4e0e78b4093a827b6b8cfd3de0ec1501dd
Shah rahman <toshah.rahman@gmail.com>
2022-03-26 13:52:33 +0200

    refined library step 1

--- Library/PyLib/PlaywrightCore.py
+++ Library/PyLib/PlaywrightCore.py
@@ -12,16 +12,22 @@ class PlaywrightCore:
         print("Opening browser : "+browser_name)
         PlaywrightCore.pwSync = sync_playwright().start()
         if browser_name.lower() == 'chromium':
-            PlaywrightCore.browser = PlaywrightCore.pwSync.chromium.launch(headless=False,slow_mo=1000)
+            PlaywrightCore.browser = PlaywrightCore.pwSync.chromium.launch(headless=False, slow_mo=1000)
+        elif browser_name.lower() == 'firefox':
+            PlaywrightCore.browser = PlaywrightCore.pwSync.firefox.launch(headless=False, slow_mo=1000)
+        else:
+            PlaywrightCore.browser = PlaywrightCore.pwSync.webkit.launch(headless=False, slow_mo=1000)
+
+        PlaywrightCore.context = PlaywrightCore.browser.new_context()
 
     @staticmethod
     def close_browser():
+        PlaywrightCore.context.close()
         PlaywrightCore.browser.close()
         PlaywrightCore.pwSync.stop()
 
     @staticmethod
     def open_application():
-        PlaywrightCore.context = PlaywrightCore.browser.new_context()
         PlaywrightCore.page = PlaywrightCore.context.new_page()
         PlaywrightCore.page.goto('https://demo.seleniumeasy.com/')
         PlaywrightCore.page.click("text=No, thanks!")
@@ -29,7 +35,6 @@ class PlaywrightCore:
     @staticmethod
     def close_application():
         PlaywrightCore.page.close()
-        PlaywrightCore.context.close()
 
     @staticmethod
     def get_page_object():
