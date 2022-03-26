dd669010baae63b6d0855fd285ecde16ded0266c
Shah rahman <toshah.rahman@gmail.com>
2022-03-26 10:29:12 +0200

    debug feature with pause

--- main.py
+++ main.py
@@ -4,9 +4,29 @@ from playwright.sync_api import sync_playwright
 p = sync_playwright().start()
 
 browser = p.chromium.launch(headless=False)
-page = browser.new_page()
+context = browser.new_context()
+page = context.new_page()
+
 page.goto("https://demo.seleniumeasy.com/")
 page.screenshot(path="example.png")
+
+#page.pause()
+# Click text=No, thanks!
+page.locator("text=No, thanks!").click()
+# Click #treemenu >> text=Input Forms
+page.locator("#treemenu >> text=Input Forms").click()
+# Click #treemenu >> text=Simple Form Demo
+page.locator("#treemenu >> text=Simple Form Demo").click()
+# expect(page).to_have_url("https://demo.seleniumeasy.com/basic-first-form-demo.html")
+# Click [placeholder="Please enter your Message"]
+page.locator("[placeholder=\"Please enter your Message\"]").click()
+# Fill [placeholder="Please enter your Message"]
+page.locator("[placeholder=\"Please enter your Message\"]").fill("test msg")
+# Click button:has-text("Show Message")
+page.locator("button:has-text(\"Show Message\")").click()
+
+page.close()
+context.close()
 browser.close()
 
 p.stop()
