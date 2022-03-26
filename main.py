a72398308abbd789f3962c3c7b1d2eec0d0e96e1
Shah rahman <toshah.rahman@gmail.com>
2022-03-26 09:25:53 +0200

    sync initialization refactor

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://demo.seleniumeasy.com/")
-    page.screenshot(path="example.png")
-    browser.close()
+#with sync_playwright() as p:
+p = sync_playwright().start()
+
+browser = p.chromium.launch(headless=False)
+page = browser.new_page()
+page.goto("https://demo.seleniumeasy.com/")
+page.screenshot(path="example.png")
+browser.close()
+
+p.stop()
