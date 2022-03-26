f27bc90b8d21143b66c0d30458c1a970305e79ba
Shah rahman <toshah.rahman@gmail.com>
2022-03-26 12:57:51 +0200

    core library step 1

---
+++ Library/PyLib/PlaywrightCore.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import sync_playwright
+
+
+class PlaywrightCore:
+    browser = None
+    context = None
+    page = None
+    pwSync = None
+
+    @staticmethod
+    def launch_browser(browser_name):
+        print("Opening browser : "+browser_name)
+        PlaywrightCore.pwSync = sync_playwright().start()
+        if browser_name.lowser() == 'chromium':
+            PlaywrightCore.browser = PlaywrightCore.pwSync.chromium.launch(headless=False,slow_mo=1000)
+
